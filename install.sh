05d7c07f31d70b090e1b16966571c1ec342150ce
Jason Peacock <jpeacock@zenoss.com>
2021-02-04 22:57:23 -0600

    Use a better method to find site-packages.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+SITE_PACKAGES=$(python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")
+
 pip --no-color --no-python-version-warning install --no-index --no-cache-dir --find-links=./wheelhouse -r ./requirements.txt wheel
-cat patches/* | patch -d $(python -c "import pip, os.path; print os.path.dirname(pip.__path__[0])") -p0
+cat patches/* | patch -d ${SITE_PACKAGES} -p0
